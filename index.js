08d63306b22c522825fb6b300a4035e88fc70801
nishithak212 <knishitha95@gmail.com>
2025-03-19 18:32:26 -0400

    Verify CORS setup

--- index.js
+++ index.js
@@ -11,19 +11,40 @@ import notificationsRouter from "./routes/notifications-routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5050;
-const CORS_ORIGIN = process.env.CORS_ORIGIN;
+const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:5173";
+
+//Enable CORS
+app.use(
+  cors({
+    origin: CORS_ORIGIN,
+    credentials: true,
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
+
+//Middleware to parse JSON requests
+app.use(express.json());
 
 //Configure session middleware
 app.use(
   session({
-    secret:process.env.SESSION_SECRET || "defaultsecret",
-    resave:false,
-    saveUninitialized:false,
-    cookie:{secure:false, httpOnly: true, maxAge: 24 * 60 * 60* 1000}, //24 hr expiry
-  }));
-  
-app.use(cors({ origin: CORS_ORIGIN }));
-app.use(express.json());
+    secret: process.env.SESSION_SECRET || "defaultsecret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      sameSite: "lax",
+      maxAge: 24 * 60 * 60 * 1000,
+    }, //24 hr validity
+  })
+);
+
+app.use((req, res, next) => {
+  console.log("Session Data:", req.session);
+  next();
+});
 
 // basic home route
 app.get("/", (_req, res) => {
@@ -32,8 +53,8 @@ app.get("/", (_req, res) => {
 
 app.use("/api/users", usersRouter); //Register user routes
 app.use("/api/grocery", groceryItemsRouter);
-app.use("/api/shopping-list",shoppingListRouter);
-app.use("/api/notifications",notificationsRouter);
+app.use("/api/shopping-list", shoppingListRouter);
+app.use("/api/notifications", notificationsRouter);
 
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`);
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,11 +1,15 @@
-const authenticateUser = (req,res,next) => {
-    console.log("Session in authMiddleware:", req.session);
-    if(!req.session || !req.session.user_id){
-        return res.status(401).json({error:"Unauthorized: User is not logged in"});
-    }
+const authenticateUser = (req, res, next) => {
+  console.log("Session in authMiddleware:", req.session);
+  if (!req.session || !req.session.user_id) {
+    console.log("No user_id is found in session");
+    return res
+      .status(401)
+      .json({ error: "Unauthorized: User is not logged in" });
+  }
 
-    req.user_id=req.session.user_id; //to attach user_id to request object
-    next();
+  console.log("User authenticated:", req.session.user_id);
+  req.user_id = req.session.user_id; //to attach user_id to request object
+  next();
 };
 
 export default authenticateUser;
